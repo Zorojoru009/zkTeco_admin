d02de29e7b9411202c71f12f6d636a1194d14a95
Hadden <abaricia76@gmail.com>
2024-07-19 13:32:06 +0800

    changes in logic

--- api_module/process.php
+++ api_module/process.php
@@ -102,20 +102,35 @@
             $check_data = $check->fetch();
             // Check if any rows were returned
             if(!$check_data) { // If no rows returned
-                // Prepare the INSERT query with placeholders
-                $get_attendance_id_num = $conn->prepare("SELECT * FROM tbl_attendance WHERE id_num = ? ORDER BY date_time DESC LIMIT 1");
-                $get_attendance_id_num->execute([$id_num]);
-                $latest_attendance_id_num = $get_attendance_id_num->fetch();
+                // Prepare the INSERT query with placeholderss
+                $today = date('Y-m-d');
+                $get_attendance_id_num = $conn->prepare("SELECT * FROM tbl_attendance WHERE id_num = ? AND date_time LIKE ? ORDER BY date_time DESC LIMIT 1");
+                $today = $today . '%';
+                $get_attendance_id_num->execute([$id_num, $today]); 
 
+              
+
+                // Execute the SELECT query
+                $latest_attendance_id_num = $get_attendance_id_num->fetch();
+              echo "latest id num $id_num :"; print_r($latest_attendance_id_num); 
                 if($get_attendance_id_num->rowCount() > 0){
                     $is_log = $latest_attendance_id_num['log_type'];
+
                     // Cgecj uf the previous data is login  so insert logout
                     if(($is_log == 0 && $log_type == 1) || ($is_log == 1 && $log_type == 0)){
                         $insertAttendance = $conn->prepare("INSERT INTO tbl_attendance (id_num, log_type, date_time) VALUES (?, ?, ?)");
                         // Execute the INSERT query with values
                         $insertAttendance->execute([$id_num, $log_type, $date]);
                     }
-                } 
+                } else {
+                    if($log_type == 0){
+                        $insertAttendance = $conn->prepare("INSERT INTO tbl_attendance (id_num, log_type, date_time) VALUES (?, ?, ?)");
+                        // Execute the INSERT query with values
+                        $insertAttendance->execute([$id_num, $log_type, $date]);
+
+                    }
+
+                }
                 // Get the latest data of the id num
                 // Insert the latest attendance data to the tbl_attendance
        
